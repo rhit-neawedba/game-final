927d09d32604b9a672090c9dd91b25276a79f350
neawedba <neawedba@rose-hulman.edu>
2025-10-30 16:23:08 -0400

    Re-optimized Enemy tracking to improve game performance later

--- src/helloWorld/Enemy.java
+++ src/helloWorld/Enemy.java
@@ -21,7 +21,7 @@ public class Enemy extends Entity {
 	int y;
 	int width;
 	int height;
-	Color color = new Color(100,200,200);
+	Color color = new Color(107,67,173); //change to close to enemy color later
 	private BufferedImage sprite;
     private boolean spriteLoaded = false;
 	
@@ -71,22 +71,38 @@ public class Enemy extends Entity {
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	/**
+	 * trackPlayer
+	 * Controls all autonomous movement of the Enemy
+	 * Help on the delays from:
+	 * https://stackoverflow.com/questions/23283041/how-to-make-java-delay-for-a-few-seconds
+	 * @param player
+	 */
 	public void trackPlayer(Player player) {
-//		if (player.getPosition() <= attackRadius && player.getX() < Enemy.getX()) {
+//		//fall if off platform - Enemy cannot jump to add a level of strategy to the player's gameplay
+//		this.applyPhysics(null); // may have to redo this
+//		
+//		//chase player
+//		if (attackRadius <= Math.sqrt(Math.abs(this.getX() - player.getX()) + Math.abs(this.getY() - player.getY())) 
+//				&& player.getX() > this.getX()) {
 //			x += speed;
 //		}
-//		else if (player.getPosition() <= attackRadius && player.getX() < Enemy.getX()) {
+//		else if (attackRadius <= Math.sqrt(Math.abs(this.getX() - player.getX()) + Math.abs(this.getY() - player.getY()))  
+//				&& player.getX() < this.getX()) {
 //			x -= speed;
 //		}
+//		
+//		//patrol pattern
 //		else {
 //			for(int i = 0; i < idleRadius; i++) {
 //				x+= speed;
 //			}
-//			//delay
+//			Thread.sleep(25);
 //			for(int i = 0; i < idleRadius; i++) {
 //				x-= speed;
 //			}
-//			//delay
+//			Thread.sleep(25);
 //		}
 	}
 	
@@ -96,6 +112,11 @@ public class Enemy extends Entity {
 //		}
 	}
 	
+	/**
+	 * draw
+	 * Draws the enemy, using the sprite if it loads, and a colored rectangle similar to it if not
+	 * @param g2
+	 */
 	public void draw(Graphics2D g2) {
 		if (spriteLoaded) {
     		g2.drawImage(sprite, x, y, width, height, null);
