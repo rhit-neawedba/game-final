5e9a99253f056d2d477896c6fcd8180594a9dd97
nelsonmp <nelsonmp@rose-hulman.edu>
2025-10-30 16:15:23 -0400

    Blank white window is working now

--- src/helloWorld/GameLauncher.java
+++ src/helloWorld/GameLauncher.java
@@ -5,11 +5,6 @@ import javax.swing.SwingUtilities;
 public class GameLauncher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		SwingUtilities.invokeLater(() -> new Game().show());
-		
-
 	}
-
 }
