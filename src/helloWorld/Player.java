ad1616cd7c33f68c7cae0a5d9dc52936b46d3542
Anthony Dunn <antdun29@gmail.com>
2025-11-03 16:12:54 -0500

    Player Sprite Creation

--- src/helloWorld/Player.java
+++ src/helloWorld/Player.java
@@ -3,6 +3,10 @@ package helloWorld;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * @author Skye Willis
@@ -10,9 +14,23 @@ import java.awt.event.KeyEvent;
 public class Player extends Entity {
 	private static final int PLAYER_HEIGHT=50, PLAYER_WIDTH=30;
 	private static final double JUMP_VELOCITY = -800;
-
+	
+	private BufferedImage sprite;
+	boolean spritecreated;
+	
+	
 	public Player(int x, int y, GamePanel canvas) {
 		super(x, y, PLAYER_WIDTH, PLAYER_HEIGHT, canvas);
+		
+		//Player Sprite Creation @Anthony Dunn
+		try {
+			sprite = ImageIO.read(Player.class.getResource("rosie.png"));
+			spritecreated = true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			spritecreated = false;
+			//e.printStackTrace();
+		}
 	}
 	
 	public void move(int dirx) {
@@ -35,10 +53,15 @@ public class Player extends Entity {
 
 	public void draw(Graphics2D g2) {
 		Color c = g2.getColor();
+		if(spritecreated) {
+    		
+    		g2.drawImage(sprite, getX(), getY(), width, height, null);
+		}
+		else {
 		g2.setColor(Color.RED);
 		g2.fillRect(getX(), getY(), width, height);
 //		g2.drawString(Boolean.toString(grounded), 100, 100);
-
+		}
 		// reset color
 		g2.setColor(c);		
 	}
