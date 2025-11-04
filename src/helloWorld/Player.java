dc8c77fd9f38516d05166be9647ee063e0c02b97
Anthony Dunn <antdun29@gmail.com>
2025-11-04 16:23:42 -0500

    Player sprite updating

--- src/helloWorld/Player.java
+++ src/helloWorld/Player.java
@@ -3,16 +3,41 @@ package helloWorld;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * @author Skye Willis
  */
+
+
+
+
 public class Player extends Entity {
 	private static final int PLAYER_HEIGHT=50, PLAYER_WIDTH=30;
 	private static final double JUMP_VELOCITY = -1200;
 
+	private BufferedImage sprite;
+	private BufferedImage leftSprite;
+	private BufferedImage rightSprite;
+	private BufferedImage jumpSprite;
+	
+	boolean spritecreated;
+	
+	
 	public Player(int x, int y, GamePanel canvas) {
 		super(x, y, PLAYER_WIDTH, PLAYER_HEIGHT, canvas);
+		
+		try {
+			sprite = ImageIO.read(Player.class.getResource("rosie.png"));
+			spritecreated = true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			spritecreated = false;
+			//e.printStackTrace();
+		}
 	}
 	public void move(int dirx) {
 		this.vx = dirx * this.speed;
@@ -36,6 +61,37 @@ public class Player extends Entity {
 
 	public void draw(Graphics2D g2) {
 		Color c = g2.getColor();
+		
+		if(spritecreated) {
+    		if(vx < 0) {
+    			try {
+					leftSprite = ImageIO.read(Player.class.getResource("rosieLeft.png"));
+					g2.drawImage(leftSprite, getX(), getY(), width, height, null);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}
+    		else if (vx > 0){
+    			try {
+					rightSprite = ImageIO.read(Player.class.getResource("rosieRight.png"));
+					g2.drawImage(rightSprite, getX(), getY(), width, height, null);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}
+    		else if (Math.abs(vy) > 0){
+    			try {
+					jumpSprite = ImageIO.read(Player.class.getResource("rosieJump.png"));
+					g2.drawImage(jumpSprite, getX(), getY(), width, height, null);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}
+		}
+		else {
 		g2.setColor(Color.RED);
 		g2.fillRect(getX(), getY(), width, height);
 //		g2.drawString(Boolean.toString(grounded), 100, 100);
@@ -44,6 +100,7 @@ public class Player extends Entity {
 		g2.setColor(c);
 //		drawHitbox(g2);
 //		d(g2);
-	}
+		}
+    		}
 
 }
