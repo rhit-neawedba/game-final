185ea9920ed3f2d2a2f976889b3007b269ce13fe
Skye <skyewells675@gmail.com>
2025-11-12 22:05:39 -0500

    tweaked player movement to make it more reliable

--- src/helloWorld/Controller.java
+++ src/helloWorld/Controller.java
@@ -8,17 +8,19 @@ public class Controller extends KeyAdapter {
 	
 	private Player player;
 	private boolean shooting;
+	private boolean[] moveAxis;
 	
 	public Controller(Player player) {
 		this.player = player;
+		this.moveAxis = new boolean[2];
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		super.keyPressed(e);
 		switch (e.getKeyCode()) {
-			case KeyEvent.VK_D -> player.move(1);
-			case KeyEvent.VK_A -> player.move(-1);
+			case KeyEvent.VK_D -> movePlayer(1, true);
+			case KeyEvent.VK_A -> movePlayer(-1, true);
 			case KeyEvent.VK_SPACE -> player.jump();
 			case KeyEvent.VK_ENTER -> { player.shoot();
 			}
@@ -33,10 +35,20 @@ public class Controller extends KeyAdapter {
 		// TODO Auto-generated method stub
 		super.keyReleased(e);
 		switch (e.getKeyCode()) {
-			case KeyEvent.VK_D -> player.move(0);
-			case KeyEvent.VK_A -> player.move(0);
+			case KeyEvent.VK_D -> movePlayer(1, false);
+			case KeyEvent.VK_A -> movePlayer(-1, false);
 			case KeyEvent.VK_S -> player.setPressingDown(false);
 		}
+	}
+	
+	private void movePlayer(int dir, boolean enabled) {
+		if (dir > 0) this.moveAxis[1] = enabled;
+		else if (dir < 0) this.moveAxis[0] = enabled;
+		
+		if (this.moveAxis[0] && !this.moveAxis[1]) player.move(-1);
+		if (!this.moveAxis[0] && this.moveAxis[1]) player.move(1);
+		if (this.moveAxis[0] == this.moveAxis[1]) player.move(0);
+		
 	}
 	
 }
