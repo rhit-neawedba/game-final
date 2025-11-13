cbaf4a8a3222b083c47a1e81201df9bbab7c9798
nelsonmp <nelsonmp@rose-hulman.edu>
2025-11-13 14:59:11 -0500

    HUD now says points,not current

--- src/helloWorld/HudViewer.java
+++ src/helloWorld/HudViewer.java
@@ -18,7 +18,7 @@ public class HudViewer extends JLabel {
             "<html><h2 style='margin:0;padding:0;'>Score</h2><ol>"
         );
         html.append("<li>Level: ").append(hud.getLevel()).append("</li>");
-        html.append("<li>Current: ").append(hud.getScore()).append("</li>");
+        html.append("<li>Points: ").append(hud.getScore()).append("</li>");
         html.append("<li>Health: ").append(hud.getLives()).append("</li>");
         html.append("</ol></html>");
         setText(html.toString());
